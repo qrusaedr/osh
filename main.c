604d2b78e53da965dd3b23fbcf4506cbed654cf0
qrusaedr <qrusaed@gmail.com>
2026-01-16 20:05:27 +0100

    updated exec_process

--- main.c
+++ main.c
@@ -1,14 +1,18 @@
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <sys/wait.h>
+#include <unistd.h>
+// #include <wait.h>
 
 #define INPUT_BUFFER 512
 #define MAX_ARGS 64
 
 void init_shell();
 int tokenize_cmd(char *buf[], char *input);
-void exec_process(const char *cmd[]);
+void exec_process(char *const args[]);
 
 int main() {
     init_shell();
@@ -21,7 +25,7 @@ void init_shell() {
     const size_t buffer_size = INPUT_BUFFER;
     while (true) {
         char input[buffer_size];
-        char *command[MAX_ARGS];
+        char *args[MAX_ARGS];
 
         printf("$: "); // prompt
         fgets(input, buffer_size, stdin);
@@ -31,13 +35,9 @@ void init_shell() {
         input[input_len - 1] = '\0';
 
         int token_len;
-        token_len = tokenize_cmd(command, input); // tokenize command
+        token_len = tokenize_cmd(args, input); // tokenize command
 
-        // print command tokens to stdout
-        for (int i = 0; i < token_len - 1; i++) {
-            printf("%s\n", command[i]);
-        }
-
+        exec_process(args);
     }
 }
 
@@ -46,13 +46,12 @@ void init_shell() {
 */
 int tokenize_cmd(char *buf[], char *input) {
     const char *delim = " ";
-    char *token =  strtok((char *)input, delim);
+    char *token =  strtok(input, delim);
 
     int i = 0;
     while (token != NULL) {
-        buf[i] = token;
+        buf[i++] = token;
         token = strtok(NULL, delim);
-        i++;
     }
 
     buf[i++] = NULL;
@@ -60,9 +59,23 @@ int tokenize_cmd(char *buf[], char *input) {
 }
 
 /*
- * Creates and Replaces the new process with cmd specifies in cmd
- * cmd is a NULL terminated array where cmd[0] is the program name
+ * Creates and Replaces the new process with cmd specifies in args
+ * args is a NULL terminated array where args[0] is the program name
  */
-void exec_process(const char *cmd[]) {
+void exec_process(char *const args[]) {
+    pid_t pid = fork();
+    if (pid == -1) {
+        perror("process");
+        exit(1);
+    }
 
+    if (pid == 0) {
+        int n = execvp(args[0], args);
+        if (n == -1) {
+            perror("exec");
+            exit(1);
+        }
+    } else {
+        wait(NULL);
+    }
 }
